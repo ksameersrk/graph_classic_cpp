7237b20a82ac519fe2ba2b7e358805460ff781e0
Rohan <rohanagarwal6@gmail.com>
2016-04-24 03:58:39 +0530

    lots of changes to graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -3,15 +3,17 @@
 #include <map>
 #include <algorithm>
 //#include <stack>
-//#include <cstring>
 using namespace std;
 
 class Node
 {
     private:
+		static int num_of_nodes;
         int name_;
+		string val_;
         map<int, int> neighbours_;
-        friend class Graph;
+        friend class Graph;	
+		friend class dfs_iterator;
  
 	//constructor       
     public:
@@ -23,7 +25,11 @@ class Node
                 neighbours_.insert(make_pair(i, v[i]));
             }
         }
-        
+        Node(string val) : val_(val) 
+		{
+			name_ = num_of_nodes++;
+		}
+		Node();
         ~Node()
         {
         }
@@ -74,25 +80,25 @@ class Node
             }
             return *this;
         }
-        /*
-		int operator*(const Node& nd)
+		bool operator==(const Node& rhs)
 		{
-			return nd.name_;
+			return name_ == rhs.name_;
 		}
-		*/
 };
-
+int Node::num_of_nodes = 0;
 class Graph
 {
     private:
         vector<Node> graph_;
         Node source_;
         Node destination_;
+		int N_;
+		vector<vector<int>> adj_matrix_;
         //Node* source_;
         //Node* destination_;
     public:
-        Graph(const vector<vector<int>>& matrix,
-                Node source=Node(), Node destination=Node()) 
+        /*Graph(const vector<vector<int>>& matrix,
+                const Node& source=Node(), const Node& destination=Node()) 
         : source_(source), destination_(destination)
         {
             for(int i=0; i<matrix.size(); i++)
@@ -100,8 +106,22 @@ class Graph
                 Node tmp(i, matrix[i]);
                 graph_.push_back(tmp);
             }
-        }
+        }*/
+
+		Graph(const int N, const Node& source, const Node& destination)
+		: N_(N), source_(source), destination_(destination)
+		{
+			for(int i=0; i<N_; i++)
+			{
+				vector<int> v{-1,-1,-1,-1}; // use iota
+				adj_matrix_.push_back(v);
+			}
+		}
         
+		void add(const Node& sc, const Node& dest, int edge_wt)
+		{	
+			adj_matrix_[sc.name_][dest.name_] = edge_wt;
+		}
         void display()
         {
             for(Node n : graph_)
@@ -114,25 +134,27 @@ class Graph
                 cout << "\n";
             }
         }
-		/*
-		dfs_iterator begin()
+		void display2()
 		{
-			return dfs_iterator(source);
+			for(int i=0; i<N_; i++)
+			{
+				for(int j=0; j<N_; j++)
+				{
+					cout << adj_matrix_[i][j] << " ";
+				}
+				cout << "\n";
+			}
 		}
-		dfs_iterator end()
-		{
-			return dfs_iterator(destination);
-		}
-		*/
+		
 		class dfs_iterator
 		{
 			private:
-			Node* sc;
+			Node sc;
 			//stack<Node*> s;
 
 			public:
 			dfs_iterator() { } 
-			dfs_iterator(Node* p) : sc(p) { }
+			dfs_iterator(const Node& p) : sc(p) { }
 			
 			//dereferencing			
 			/*Node operator*()
@@ -141,11 +163,13 @@ class Graph
 			}*/
 			int operator*()
 			{
-				return sc->name_;
+				cout << sc.val_ << "\n";
+				return sc.name_;
 			}
+			
 			bool operator==(dfs_iterator& rhs)
 			{
-				return sc == rhs.sc;
+				return sc == rhs.sc; // this.operator==(node& rhs);
 			}
 			bool operator!=(dfs_iterator& rhs)
 			{
@@ -153,6 +177,14 @@ class Graph
 			}
 		};	
 
+		dfs_iterator begin()
+		{
+			return dfs_iterator(source_);
+		}
+		dfs_iterator end()
+		{
+			return dfs_iterator(destination_);
+		}
 };
 
 vector<vector<int>> input(int N)
@@ -172,6 +204,18 @@ vector<vector<int>> input(int N)
     return v;
 }
 
+template <typename ptr_t >
+void check_iterator(ptr_t first, ptr_t last)
+{
+	cout << "here\n";
+	cout << *first << " " << *last << "\n";
+	/*	
+	if(first == last)
+		cout << "here\n";
+	else if(first != last)
+		cout << "here2\n";
+	*/
+}
 int main()
 {
     int N;
@@ -180,7 +224,22 @@ int main()
     //Graph g(adjacency_matrix);
     //g.display();
 
-	Graph g();
-	g.add(Node(),Node(),3);
+	Node sc("bnglre");
+	Node dest("delhi");
+	Graph g(N,sc,dest);
+	
+	//g.display2();
+	Node a("mumbai");
+	Node b("chennai");
+
+	g.add(sc,a,1);
+	g.add(a,b,2);
+	g.add(b,a,3);
+	g.add(dest,b,4);
+	g.add(sc,dest,5);
+	
+	//g.display2();
+
+	check_iterator(g.begin(),g.end());
     return 0;
 }
