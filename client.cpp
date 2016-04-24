addd47a60a797cc143d73bd9a1ce9f2498e52700
anirudhagar13 <anirudhagarwal13@gmail.com>
2016-04-25 02:05:54 +0530

    End corrected and topological sort

--- client.cpp
+++ client.cpp
@@ -59,32 +59,13 @@ int main()
     vector<string> vertex_names = input_string(N);
     vector<vector<int>> adjacency_matrix = input_matrix(N);
     Graph g(adjacency_matrix, vertex_names);
+
     #if 0
-    cout << "BFS : ";
-    disp(g.begin_bfs("Mumbai"), g.end_bfs("Belgaum"));
+    /*cout << "BFS : ";
+    disp(g.begin_bfs("Belgaum"), g.end_bfs("Chennai"));*/
     cout << "DFS : ";
-    disp(g.begin_dfs("Mumbai"), g.end_dfs("Belgaum"));
-    #endif
-    
-    #if 0
-    cout << "BFS : ";
-    disp(g.begin_bfs("Belgaum"), g.end_bfs("Mumbai"));
-    cout << "DFS : ";
-    disp(g.begin_dfs("Belgaum"), g.end_dfs("Mumbai"));
-    #endif
-    
-    #if 0
-    cout << "BFS : ";
-    disp(g.begin_bfs("Belgaum"), g.end_bfs("Chennai"));
-    //cout << "DFS : ";
-    //disp(g.begin_dfs("Belgaum"), g.end_dfs("Mumbai"));
-    #endif
-    
-    #if 1
-    cout << "BFS : ";
-    disp(g.begin_bfs("Belgaum"), g.end_bfs("Chennai"));
-    cout << "DFS : ";
-    disp(g.begin_dfs("Belgaum"), g.end_dfs("Chennai"));
+    disp(g.begin_dfs("Belgaum"), g.end_dfs());
+    //vector<string> test= g.topo_sort(g.begin_dfs("Belgaum"), g.end_dfs("Chennai"));
     #endif
     
     return 0;
