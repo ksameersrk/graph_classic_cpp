3e0689dc1f767431887ad7c71e5885f5d46b0fc4
Rohan <rohanagarwal6@gmail.com>
2016-04-24 14:31:05 +0530

    dfs

--- graph.cpp
+++ graph.cpp
@@ -2,21 +2,24 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-//#include <stack>
+#include <list> 
+//#include <cstring>
 using namespace std;
 
 class Node
 {
     private:
-		static int num_of_nodes;
         int name_;
-		string val_;
         map<int, int> neighbours_;
-        friend class Graph;	
-		friend class dfs_iterator;
- 
-	//constructor       
+        friend class Graph;
+        
     public:
+    	//default constructor
+    	Node()
+    	:name_(-1)
+    	{}
+
+    	//parametrized constructor
         Node(int name, const vector<int>& v)
         : name_(name)
         {
@@ -25,11 +28,8 @@ class Node
                 neighbours_.insert(make_pair(i, v[i]));
             }
         }
-        Node(string val) : val_(val) 
-		{
-			name_ = num_of_nodes++;
-		}
-		Node();
+        
+        //destructor
         ~Node()
         {
         }
@@ -69,6 +69,7 @@ class Node
             //rhs.neighbours_ = NULL;
         }
         
+        //move assignment
         Node& operator=(Node&& rhs)
         {
             if(this != &rhs)
@@ -80,48 +81,27 @@ class Node
             }
             return *this;
         }
-		bool operator==(const Node& rhs)
-		{
-			return name_ == rhs.name_;
-		}
+        
 };
-int Node::num_of_nodes = 0;
+
 class Graph
 {
     private:
         vector<Node> graph_;
-        Node source_;
-        Node destination_;
-		int N_;
-		vector<vector<int>> adj_matrix_;
-        //Node* source_;
-        //Node* destination_;
+        int source_;
+        int destination_;
     public:
-        /*Graph(const vector<vector<int>>& matrix,
-                const Node& source=Node(), const Node& destination=Node()) 
-        : source_(source), destination_(destination)
+        Graph(const vector<vector<int>>& matrix,
+                int src = 0,int dest = 0) 
+        : source_(src), destination_(dest)
         {
             for(int i=0; i<matrix.size(); i++)
             {
                 Node tmp(i, matrix[i]);
                 graph_.push_back(tmp);
             }
-        }*/
-
-		Graph(const int N, const Node& source, const Node& destination)
-		: N_(N), source_(source), destination_(destination)
-		{
-			for(int i=0; i<N_; i++)
-			{
-				vector<int> v{-1,-1,-1,-1}; // use iota
-				adj_matrix_.push_back(v);
-			}
-		}
-        
-		void add(const Node& sc, const Node& dest, int edge_wt)
-		{	
-			adj_matrix_[sc.name_][dest.name_] = edge_wt;
-		}
+        }
+        /*
         void display()
         {
             for(Node n : graph_)
@@ -134,56 +114,109 @@ class Graph
                 cout << "\n";
             }
         }
-		void display2()
-		{
-			for(int i=0; i<N_; i++)
-			{
-				for(int j=0; j<N_; j++)
-				{
-					cout << adj_matrix_[i][j] << " ";
-				}
-				cout << "\n";
-			}
-		}
-		
+		*/
+/*
+        //bfs iterator
+        class bfs_iterator
+        {
+        	private:
+        	Node *pt_;
+
+        	//constructor
+        	bfs_iterator(Node *rhs)
+        	:pt_(rhs){}
+
+        	//dereferencing
+        	int operator *()
+        	{
+        		return pt_->name_;
+        	}
+
+        	bool operator ==(bfs_iterator rhs)
+        	{
+        		return pt_ == rhs.pt_;
+        	}
+
+        	bool operator !=(bfs_iterator rhs)
+        	{
+        		return !(pt_ == rhs.pt_);
+        	}
+
+        	Node operator ++()
+        	{
+        		//to be done
+        	}
+
+        };
+*/
 		class dfs_iterator
 		{
 			private:
-			Node sc;
-			//stack<Node*> s;
-
+			Node* pt_;
+			vector<Node> graph_temp_;
+			//int src;	
+			int n = 4;
+			//vector<int> visited;
+			int* visited;
+			list<int> stack; 
 			public:
-			dfs_iterator() { } 
-			dfs_iterator(const Node& p) : sc(p) { }
-			
-			//dereferencing			
-			/*Node operator*()
-			{	
-				return *sc;
-			}*/
+			dfs_iterator(Node* sc, vector<Node> graph_temp)
+			:visited(new int[graph_temp.size()]), pt_(sc), graph_temp_(graph_temp)
+			{
+				//cout << pt_->name_ << "\n";
+				//pt_ = SC;		
+				//fill(visited.begin(),visited.end(),-1);
+				for(int i=0; i<graph_temp.size(); i++)
+					visited[i] = -1;
+			}
 			int operator*()
 			{
-				cout << sc.val_ << "\n";
-				return sc.name_;
+				return pt_->name_;
 			}
+			bool operator==(const dfs_iterator& rhs)
+			{
+				return pt_->name_ == rhs.pt_->name_;
+			}
+			bool operator!=(const dfs_iterator& rhs)
+			{
+				//return !(pt_->name_ == rhs.pt_->name_);
+				return !(*this == rhs);
+			}
+
+			#if 1
+			dfs_iterator& operator++() // pre
+			{
+				if(visited[pt_->name_] == -1)
+					visited[pt_->name_] = 1;
+				vector<int> temp;
+				for(auto k : pt_->neighbours_)
+				{
+					if(k.second > 0)
+						temp.push_back(k.first);
+				}
+		
+				for(int i : temp) 
+				{
+					if(visited[i] == -1)
+						stack.push_back(i);
+				}
+				pt_ = &graph_temp_[stack.back()];
+				cout << "pt_.name_ = " << pt_->name_ << "\n";
+				stack.pop_back();
+				return *this;
+			}
+			#endif		
 			
-			bool operator==(dfs_iterator& rhs)
-			{
-				return sc == rhs.sc; // this.operator==(node& rhs);
-			}
-			bool operator!=(dfs_iterator& rhs)
-			{
-				return !(sc == rhs.sc);
-			}
-		};	
+			
+		};
 
 		dfs_iterator begin()
 		{
-			return dfs_iterator(source_);
+			return dfs_iterator(&graph_[source_],graph_);
 		}
 		dfs_iterator end()
 		{
-			return dfs_iterator(destination_);
+			return dfs_iterator(&graph_[destination_],graph_);
 		}
 };
 
@@ -204,42 +237,31 @@ vector<vector<int>> input(int N)
     return v;
 }
 
-template <typename ptr_t >
-void check_iterator(ptr_t first, ptr_t last)
-{
-	cout << "here\n";
-	cout << *first << " " << *last << "\n";
-	/*	
-	if(first == last)
-		cout << "here\n";
-	else if(first != last)
-		cout << "here2\n";
-	*/
-}
 int main()
 {
     int N;
     cin >> N;
-    //vector<vector<int>> adjacency_matrix = input(N);
-    //Graph g(adjacency_matrix);
+    //names mapping
+    vector<vector<int>> adjacency_matrix = input(N);
+    Graph g(adjacency_matrix,0,2);
     //g.display();
-
-	Node sc("bnglre");
-	Node dest("delhi");
-	Graph g(N,sc,dest);
-	
-	//g.display2();
-	Node a("mumbai");
-	Node b("chennai");
-
-	g.add(sc,a,1);
-	g.add(a,b,2);
-	g.add(b,a,3);
-	g.add(dest,b,4);
-	g.add(sc,dest,5);
-	
-	//g.display2();
-
-	check_iterator(g.begin(),g.end());
-    return 0;
+	auto first = g.begin();
+	auto last = g.end();
+	cout << *first << "\n";
+	cout << *last << "\n";
+/*
+	++first;
+	cout << *first << "\n";   
+	++first;
+	cout << *first << "\n";   
+	++first;
+	cout << *first << "\n";   
+*/
+	do//while(first != last)
+	{
+		cout << "first = " <<  *first << "\n";
+		++first;
+	}while(first != last)
+	//cout << "first = " << *first << "\n";
+	return 0;
 }
